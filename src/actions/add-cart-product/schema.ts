310e10a7835144ec1e04e1f66822e630cc4d671f
Infinity <wemersoncontas22@gmail.com>
2025-08-18 11:10:25 -0300

    feat: add product to cart action

---
+++ src/actions/add-cart-product/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const addProductToCartSchema = z.object({
+  productVariantId: z.uuid(),
+
+  quantity: z.number().min(1),
+});
+
+export type AddProductToCartSchema = z.infer<typeof addProductToCartSchema>;
--- src/app/components/common/cart.tsx
+++ src/app/components/common/cart.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ShoppingBasketIcon } from "lucide-react";
 
 import { Button } from "../ui/button";
