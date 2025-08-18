7f30bf45e48680f3106901bf5bb211a8da6389bf
Infinity <wemersoncontas22@gmail.com>
2025-08-18 20:51:19 -0300

    feat: add checkout success page

---
+++ src/app/checkout/success/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Header } from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const CheckoutSuccessPage = () => {
+  return (
+    <>
+      <Header />
+      <Dialog open={true} onOpenChange={() => {}}>
+        <DialogContent className="text-center">
+          <Image
+            src="/illustration.svg"
+            alt="Success"
+            width={300}
+            height={300}
+            className="mx-auto"
+          />
+          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
+          <DialogDescription className="font-medium">
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button className="rounded-full" size="lg">
+              Ver meus pedidos
+            </Button>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="lg"
+              asChild
+            >
+              <Link href="/">Voltar para a loja</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default CheckoutSuccessPage;
