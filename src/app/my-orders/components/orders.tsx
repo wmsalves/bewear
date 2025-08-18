a8ebc2ab8e88a613eb70e6a46cc4e2bb3130fad4
Infinity <wemersoncontas22@gmail.com>
2025-08-18 20:54:08 -0300

    feat: implement order confirmation functionality

---
+++ src/app/my-orders/components/orders.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import Image from "next/image";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { orderTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+
+interface OrdersProps {
+  orders: Array<{
+    id: string;
+
+    totalPriceInCents: number;
+
+    status: (typeof orderTable.$inferSelect)["status"];
+
+    createdAt: Date;
+
+    items: Array<{
+      id: string;
+
+      imageUrl: string;
+
+      productName: string;
+
+      productVariantName: string;
+
+      priceInCents: number;
+
+      quantity: number;
+    }>;
+  }>;
+}
+
+const Orders = ({ orders }: OrdersProps) => {
+  return (
+    <div className="space-y-5">
+      {orders.map((order) => (
+        <Card key={order.id}>
+          <CardContent>
+            <Accordion type="single" collapsible key={order.id}>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  <div className="flex flex-col gap-1">
+                    {order.status === "paid" && <Badge>Pago</Badge>}
+
+                    {order.status === "pending" && (
+                      <Badge variant="outline">Pagamento pendente</Badge>
+                    )}
+
+                    {order.status === "canceled" && (
+                      <Badge variant="destructive">Cancelado</Badge>
+                    )}
+
+                    <p>
+                      Pedido feito em{" "}
+                      {new Date(order.createdAt).toLocaleDateString("pt-BR")} às{" "}
+                      {new Date(order.createdAt).toLocaleTimeString("pt-BR", {
+                        hour: "2-digit",
+
+                        minute: "2-digit",
+                      })}
+                    </p>
+                  </div>
+                </AccordionTrigger>
+
+                <AccordionContent>
+                  {order.items.map((product) => (
+                    <div
+                      className="flex items-center justify-between"
+                      key={product.id}
+                    >
+                      <div className="flex items-center gap-4">
+                        <Image
+                          src={product.imageUrl}
+                          alt={product.productName}
+                          width={78}
+                          height={78}
+                          className="rounded-lg"
+                        />
+
+                        <div className="flex flex-col gap-1">
+                          <p className="text-sm font-semibold">
+                            {product.productName}
+                          </p>
+
+                          <p className="text-muted-foreground text-xs font-medium">
+                            {product.productVariantName} x {product.quantity}
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col items-end justify-center gap-2">
+                        <p className="text-sm font-bold">
+                          {formatCentsToBRL(
+                            product.priceInCents * product.quantity,
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+
+                  <div className="py-5">
+                    <Separator />
+                  </div>
+
+                  <div className="space-y-2">
+                    <div className="flex justify-between">
+                      <p className="text-sm">Subtotal</p>
+
+                      <p className="text-muted-foreground text-sm font-medium">
+                        {formatCentsToBRL(order.totalPriceInCents)}
+                      </p>
+                    </div>
+
+                    <div className="flex justify-between">
+                      <p className="text-sm">Frete</p>
+
+                      <p className="text-muted-foreground text-sm font-medium">
+                        GRÁTIS
+                      </p>
+                    </div>
+
+                    <div className="flex justify-between">
+                      <p className="text-sm">Total</p>
+
+                      <p className="text-sm font-semibold">
+                        {formatCentsToBRL(order.totalPriceInCents)}
+                      </p>
+                    </div>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Orders;
---
+++ src/app/my-orders/page.tsx
@@ -0,0 +1,72 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { Header } from "@/components/common/header";
+import { db } from "@/db";
+import { orderTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import Orders from "./components/orders";
+
+const MyOrdersPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user.id) {
+    redirect("/login");
+  }
+
+  const orders = await db.query.orderTable.findMany({
+    where: eq(orderTable.userId, session?.user.id),
+
+    with: {
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+
+      <div className="px-5">
+        <Orders
+          orders={orders.map((order) => ({
+            id: order.id,
+
+            totalPriceInCents: order.totalPriceInCents,
+
+            status: order.status,
+
+            createdAt: order.createdAt,
+
+            items: order.items.map((item) => ({
+              id: item.id,
+
+              imageUrl: item.productVariant.imageUrl,
+
+              productName: item.productVariant.product.name,
+
+              productVariantName: item.productVariant.name,
+
+              priceInCents: item.productVariant.priceInCents,
+
+              quantity: item.quantity,
+            })),
+          }))}
+        />
+      </div>
+    </>
+  );
+};
+
+export default MyOrdersPage;
